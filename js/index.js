b326716d123305f95a18dbe733b33915b5c27775
Luis Rodriguez <luisfrodriguezb.lf@gmail.com>
2021-09-24 00:53:31 -0400

    FIX: Problemas con el screen y animaciones
    El screen parpadeaba al darle click y luego se apagaba, al volverle
    a dar click, este funcionaba bien. Además, las animaciones se iban
    fueran de tiempo
    
    Se soluciono colocando en javascript play stated y
    
    La estructura tenia problemas con las animaciones. Se dio una
    reestructura comoda para que pueda llevarse mejor el progreso
    
    Resumen:
    - Resuelto los problemas de screen
    - Animaciones con la tablet y screen
    - Estructuracion
    
    Issue: #1

--- js/index.js
+++ js/index.js
@@ -1,13 +1,27 @@
 const btn = document.querySelector('#btn');
 
+const runningAnimation = (target) => {
+  target.style.animationPlayState = "running";
+}
+
+const runningAnimationAll = (target) => {
+  target.forEach(element => element.style.animationPlayState = "running");
+}
+
 btn.addEventListener('click', (event) => {
   const tabletBtn = event.target;
   const tablet = document.querySelector("#tablet");
   const screen = document.querySelector("#screen");
-
   if(tabletBtn.checked) {
+    const screenText = document.querySelector('#text');
+    const screenTextElement = screenText.querySelectorAll('span');
+
     tablet.style.animationName = "wideout";
     screen.style.animationName = "powerOn";
+    runningAnimation(screenText);
+    runningAnimationAll(screenTextElement);
+
+
   } else {
     screen.style.animationName = "powerOff";
   }
